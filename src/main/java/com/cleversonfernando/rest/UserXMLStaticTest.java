9b7e3f699980557e823f0d6ad29042b408fad6a7
Cleverson Fernando <cfrodrigues50@gmail.com>
2023-01-10 19:15:39 -0300

    Request/Response Specification

--- src/main/java/com/cleversonfernando/rest/UserXMLStaticTest.java
+++ src/main/java/com/cleversonfernando/rest/UserXMLStaticTest.java
@@ -1,5 +1,10 @@
 package com.cleversonfernando.rest;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -8,22 +13,35 @@ import static org.hamcrest.Matchers.*;
 
 public class UserXMLStaticTest {
 
+    public static RequestSpecification reqSpec;
+    public static ResponseSpecification resSpec;
+
     @BeforeClass
     public static void setup() {
         baseURI = "http://restapi.wcaquino.me/";
 //      port = 80;
 //      basePath = "";
+
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
+        requestSpecBuilder.log(LogDetail.ALL);
+        reqSpec  = requestSpecBuilder.build();
+
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder();
+        responseSpecBuilder.expectStatusCode(200);
+         resSpec = responseSpecBuilder.build();
+
+         requestSpecification = reqSpec;
+         responseSpecification = resSpec;
+
     }
     @Test
     public void devoTrabalharComXML() {
 
-
         given()
-                .log().all()
                 .when()
                 .get("/usersXML/3")
                 .then()
-                .statusCode(200)
+ //             .statusCode(200)
                 .body("user.name", is("Ana Julia"))
                 .body("user.@id", is("3"))
                 .body("user.filhos.name.size()", is(2))
