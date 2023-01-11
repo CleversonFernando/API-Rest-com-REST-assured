9dfb381bc7c5a38425dda44866653c4a88bd007a
Cleverson Fernando <cfrodrigues50@gmail.com>
2023-01-11 20:07:05 -0300

    Busca com HTML

---
+++ src/main/java/com/cleversonfernando/rest/Html.java
@@ -0,0 +1,30 @@
+package com.cleversonfernando.rest;
+
+import io.restassured.http.ContentType;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class Html {
+
+    @Test
+    public void deveFazerBuscaComHTML(){
+
+        given()
+                .log().all()
+                .when()
+                .get("https://restapi.wcaquino.me/v2/users")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .contentType(ContentType.HTML)
+                .body("html.body.div.table.tbody.tr.size()", is(3))
+                .body("html.body.div.table.tbody.tr[1].td[2]", is(25))
+                .appendRootPath("html.body.div.table.tbody")
+                .body("tr.find{it.toString().startsWith('2')}.td[1]", is("Maria Joaquina"))
+
+
+        ;
+    }
+}
