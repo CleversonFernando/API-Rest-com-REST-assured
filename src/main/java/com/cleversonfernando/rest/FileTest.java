873fd73a8e6cfe94460bc9a14e5cb100580d1cd8
Cleverson Fernando <cfrodrigues50@gmail.com>
2023-01-12 11:51:45 -0300

    Definindo tempo Máximo

--- src/main/java/com/cleversonfernando/rest/FileTest.java
+++ src/main/java/com/cleversonfernando/rest/FileTest.java
@@ -6,11 +6,12 @@ import java.io.File;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThan;
 
 public class FileTest {
 
     @Test
-    public void deveObrigarEnvioArquivo(){
+    public void deveObrigarEnvioArquivo() {
 
         given()
                 .log().all()
@@ -21,10 +22,11 @@ public class FileTest {
                 .statusCode(404)// Deveria ser 400
                 .body("error", is("Arquivo não enviado"))
 
-                ;
+        ;
     }
+
     @Test
-    public void deveFazerUploadDoArquivo(){
+    public void deveFazerUploadDoArquivo() {
 
         given()
                 .log().all()
@@ -35,6 +37,22 @@ public class FileTest {
                 .log().all()
                 .statusCode(200)
                 .body("name", is("users.pdf"))
+
+        ;
+    }
+
+    @Test
+    public void naodeveFazerUploadDeArquivoGrande() {
+
+        given()
+                .log().all()
+                .multiPart("arquivo", new File("src/main/resources/Arquivo1MB+.pdf"))
+                .when()
+                .post("https://restapi.wcaquino.me/upload")
+                .then()
+                .log().all()
+                .time(lessThan(5000L))
+                .statusCode(413)
 
         ;
     }
