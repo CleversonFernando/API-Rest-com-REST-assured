d2bc87a02264be8a24f9633d4d08f83cdc643fb9
Cleverson Fernando <cfrodrigues50@gmail.com>
2023-01-12 11:01:47 -0300

    Busca Xpath com HTML

--- src/main/java/com/cleversonfernando/rest/Html.java
+++ src/main/java/com/cleversonfernando/rest/Html.java
@@ -4,6 +4,7 @@ import io.restassured.http.ContentType;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasXPath;
 import static org.hamcrest.Matchers.is;
 
 public class Html {
@@ -24,6 +25,20 @@ public class Html {
                 .appendRootPath("html.body.div.table.tbody")
                 .body("tr.find{it.toString().startsWith('2')}.td[1]", is("Maria Joaquina"))
 
+        ;
+    } @Test
+    public void deveFazerBuscaComXpathEmHTML(){
+
+        given()
+                .log().all()
+                .when()
+                .get("https://restapi.wcaquino.me/v2/users?format=clean")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .contentType(ContentType.HTML)
+                .body(hasXPath("count(//table/tr)", is("4")))
+                .body(hasXPath("//td[text() = '2']/../td[2]", is("Maria Joaquina")))
 
         ;
     }
