82527b5f93a572368c8a793f9b8ca380dcb3b765
Cleverson Fernando <cfrodrigues50@gmail.com>
2023-01-12 14:51:36 -0300

    Download

--- src/main/java/com/cleversonfernando/rest/FileTest.java
+++ src/main/java/com/cleversonfernando/rest/FileTest.java
@@ -3,10 +3,14 @@ package com.cleversonfernando.rest;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.lessThan;
+import static org.junit.Assert.assertThat;
 
 public class FileTest {
 
@@ -55,5 +59,25 @@ public class FileTest {
                 .statusCode(413)
 
         ;
+    }
+    @Test
+    public void deveBaixarArquivo() throws IOException {
+
+        byte[] image = given()
+                .log().all()
+                .when()
+                .get("https://restapi.wcaquino.me/download")
+                .then()
+                .statusCode(200)
+                .extract().asByteArray();
+
+        File imagem = new File("src/main/resources/file.jpg");
+        OutputStream out = new FileOutputStream(imagem);
+        out.write(image);
+        out.close();
+
+  //    System.out.println(imagem.length());
+        assertThat(imagem.length(), lessThan(100000L));
+
     }
 }
