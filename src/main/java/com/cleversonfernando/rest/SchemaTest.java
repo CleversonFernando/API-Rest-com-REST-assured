90f9266452c42053f71b08c2dfb798f271b17961
Cleverson Fernando <cfrodrigues50@gmail.com>
2023-01-12 15:54:48 -0300

    Validando XML

---
+++ src/main/java/com/cleversonfernando/rest/SchemaTest.java
@@ -0,0 +1,40 @@
+package com.cleversonfernando.rest;
+
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.matcher.RestAssuredMatchers.matchesXsdInClasspath;
+
+public class SchemaTest {
+
+    @Test
+    public void deveValidarSchemaXML(){
+
+        given()
+                .log().all()
+                .when()
+                .get("https://restapi.wcaquino.me/usersXML")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body(matchesXsdInClasspath("users.xsd"))
+
+                ;
+
+    } @Test(expected = SAXException.class)
+    public void naoDeveValidarSchemaXML(){
+
+        given()
+                .log().all()
+                .when()
+                .get("https://restapi.wcaquino.me/invalidUsersXML")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body(matchesXsdInClasspath("users.xsd"))
+
+        ;
+
+    }
+}
